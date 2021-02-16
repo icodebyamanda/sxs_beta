abd606e3c9bc339fa7a8cbae7ea4641b7be45b51
icodebyamanda <rasoamanda@gmail.com>
2021-02-16 12:47:47 +0100

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState }  from "react";
+import './App.css';
+
+function App() {
+
+  const [state, setState] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
