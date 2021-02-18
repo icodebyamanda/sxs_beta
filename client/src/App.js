50686ab29bb8edb25aa6b4fbd45c74c752cf7d0f
icodebyamanda <rasoamanda@gmail.com>
2021-02-18 13:43:12 +0100

    beginning skeleton issue #14

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,59 @@
 import React, { useEffect, useState }  from "react";
 import './App.css';
+// import blessed from './emoticons/blessed.png'; 
+//import emoticons from './emoticons'; 
+
+// console.log(blessed)
 
 function App() {
 
-  const [state, setState] = useState([]);
+  const [moods, setMoodState] = useState([]);
+  const [responses, setResponseState] = useState("")
+  // response state
+  // const [sad, setSadState] = useState("");
+
+  useEffect(() => {
+    getResponses();
+  }, []);
+
+  function handleSadClick(e) {
+
+    setSadState(e.target.value);
+    console.log(e)
+
+  }
+
+  const getResponses = (moodref) => {
+    fetch(`/responses/${moodref}`)
+      .then((response) => response.json())
+      .then()
+    
+  }
 
   return (
     <div className="App">
       <header>
-      Does that work?
-      
+      React! Are you working? 
       </header>
 
-      <div>
+      {/* <div>
+        <img src="./assets/emoticons/blessed.png" alt="blessed" />;
+      </div> */}
+
+      <div 
+      name="moods"
+      value={moods}
+      >
+
+        <button 
+          name="sad"
+          value={sad}
+          onClick={handleSadClick}
+        >sad</button>
+        
+        <button>blessed</button>
 
       </div>
-
 
     </div>
 
