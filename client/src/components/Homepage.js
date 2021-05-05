78eb9c5174fac00b9322c5b8e2a99cc5a6622f6a
icodebyamanda <rasoamanda@gmail.com>
2021-05-05 18:45:13 +0200

    #25 done

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -8,10 +8,9 @@ export default function Homepage() {
     <div className="grid-container">
 
     <header className="header" id="homepageHeader">
-      <h1 id="homepageHOne"> Step By Step </h1>
+      <h1> Step By Step </h1>
       <h2 id="HomepageSubheading">
-        <div className="forSpacing"> 'Vulnerability is the birthplace of innovation, creativity, and change' </div>
-        <div className="forSpacing"> Brené Brown </div>
+        <div> 'Vulnerability is the birthplace of innovation, creativity, and change'</div> <div> by Brené Brown </div>
       </h2> 
     </header>
 
@@ -25,7 +24,7 @@ export default function Homepage() {
           <h4 className="alignLeft"> This morning tool aims at helping you acknowledge your emotions, avoid denial for which you receive back some positive vibes to get you going with your day. </h4>
 
           <p> In the <span className="spanHomepage"> 'Admin Space' </span>, store some links, videos or quotes, that inspire you, make you happy and make you feel good.<br></br>
-          You would need to match these links according to the emotion you would like them to address to. </p>
+          You would need to match these links to the emotion you would like them to address to. </p>
 
           <p> In <span className="spanHomepage"> 'Your Routine' </span>, select the emoticon that matches the closest your current emotions. <br></br>
           In return, you will receive one of these links you have previously stored.  </p>
