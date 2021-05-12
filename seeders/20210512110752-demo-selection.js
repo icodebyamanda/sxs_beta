efdabb3e9764ba5dcb008b4a4196993506f4faeb
icodebyamanda <rasoamanda@gmail.com>
2021-05-12 13:26:31 +0200

    #1 seq bis - added 4 rows seeds - testing it works

--- seeders/20210512110752-demo-selection.js
+++ seeders/20210512110752-demo-selection.js
@@ -11,6 +11,33 @@ module.exports = {
       note: "When everything falls into place, There'll be days like this",
       createdAt: new Date(),
       updatedAt: new Date()
+     }], [{
+      mood: 'sad',
+      format: 'video',
+      author: 'Jain',
+      url: 'https://youtu.be/JkVqeHpqIMk',
+      description: 'Hope',
+      note: "The most hopeful song for a sad day, Keep going",
+      createdAt: new Date(),
+      updatedAt: new Date()
+     }], [{
+      mood: 'fidgety',
+      format: 'video',
+      author: 'Harry Belafonte',
+      url: 'https://youtu.be/lewrnLwCtok',
+      description: 'Jump In the Line - BeetleJuice Song',
+      note: "Shake! Shake! Shake! And Jump in the Line!",
+      createdAt: new Date(),
+      updatedAt: new Date()
+     }], [{
+      mood: 'determined',
+      format: 'video',
+      author: 'Chuck Berry',
+      url: 'https://youtu.be/aKCt8ssC7cs',
+      description: 'Johnny B. Goode',
+      note: "Someday your name will be in lights, Saying: Johnny B. Goode tonight",
+      createdAt: new Date(),
+      updatedAt: new Date()
      }], {});
   },
 
