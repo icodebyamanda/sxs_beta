6539bb92a26d268d88be02a0ea455b60fb42d7ee
icodebyamanda <rasoamanda@gmail.com>
2021-05-12 21:54:31 +0200

    #1 youpee managed to create randomisation of a mood

--- routes/selections.js
+++ routes/selections.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const { sequelize } = require("../models");
 var router = express.Router();
 var models = require("../models");
 
@@ -12,7 +13,44 @@ router.get("/", function (req, res) {
     });
 });
 
+// Get all selections in random order
+
+//! Get all selections
+router.get("/", function (req, res) {
+  models.Selection.findAll({
+    // limit: 1,
+    order: [
+      [sequelize.fn('RANDOM')
+    ]
+    ]
+  })
+    .then((data) => res.send(data))
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
 //! GET all selections of one mood
+
+// router.get('/:mood', function(req, res) {
+
+//   const {mood} = req.params;
+
+//   models.Selection.findAll({
+//     where: {
+//       mood, 
+//     },
+//   })
+//   .then(data => {
+//     res.send(data);
+//   })
+//   .catch(err => res.status(500).send(err));
+
+//  });
+
+//! GET a random selection based on the selected user's mood
+
+// Translate from MYSQL -->> db(`SELECT * FROM responses WHERE mood = "${moodId}" ORDER BY RAND() LIMIT 1;`)
 
 router.get('/:mood', function(req, res) {
 
@@ -22,17 +60,44 @@ router.get('/:mood', function(req, res) {
     where: {
       mood, 
     },
+
+    // order: models.sequelize.random(), limit: 1
+
+    order: sequelize.random(), limit: 1
+    
   })
   .then(data => {
-    res.send(data);
+    res.send(data)[0];
   })
   .catch(err => res.status(500).send(err));
 
  });
 
-//! GET a random selection based on the selected user's mood
 
-// db(`SELECT * FROM responses WHERE mood = "${moodId}" ORDER BY RAND() LIMIT 1;`)
+
+// router.get('/:mood', function(req, res) {
+
+//   const {mood} = req.params;
+
+//   models.Selection.findAll({
+//     where: {
+//       mood, 
+//     },
+
+//     attributes: ['mood',
+//     [sequelize.fn('RANDOM', sequelize.col('mood')), 'entry']],
+//     limit: 1
+
+//   })
+// //   .then(function(rows) {
+// //     res.render('mood', { rows: rows });
+// // })
+//   .then(data => {
+//     res.send(data[0]);
+//   })
+//   .catch(err => res.status(500).send(err));
+
+//  });
 
 // router.get('/:mood', function(req, res) {
 
