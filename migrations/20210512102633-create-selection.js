333f1d47ddac8da34d40796ecf90f03b60bde654
icodebyamanda <rasoamanda@gmail.com>
2021-05-12 12:27:35 +0200

    #1 seq bis - model Selection created

---
+++ migrations/20210512102633-create-selection.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Selections', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      mood: {
+        type: Sequelize.STRING
+      },
+      format: {
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      note: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Selections');
+  }
+};
---
+++ models/selection.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Selection extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Selection.init({
+    mood: DataTypes.STRING,
+    format: DataTypes.STRING,
+    author: DataTypes.STRING,
+    url: DataTypes.STRING,
+    description: DataTypes.STRING,
+    note: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Selection',
+  });
+  return Selection;
+};
