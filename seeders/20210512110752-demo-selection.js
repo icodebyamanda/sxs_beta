cec9bcc2aa77585802dd7567bc6ecceb1c26b1e8
icodebyamanda <rasoamanda@gmail.com>
2021-05-12 13:20:04 +0200

    #1 attempting to add 4 rows of seed data

--- seeders/20210512110752-demo-selection.js
+++ seeders/20210512110752-demo-selection.js
@@ -2,23 +2,19 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('Selections', [{
+      mood: 'blessed',
+      format: 'video',
+      author: 'Van Morrison',
+      url: 'https://youtu.be/3UUWkr4FUlo',
+      description: 'Days Like This',
+      note: "When everything falls into place, There'll be days like this",
+      createdAt: new Date(),
+      updatedAt: new Date()
+     }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Selections', null, {});
   }
 };
