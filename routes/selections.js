4afa3e7eeb860bee5758f02158045dd92acacd88
icodebyamanda <rasoamanda@gmail.com>
2021-05-12 17:04:38 +0200

    #1 youpee route to one mood as params is working with seq bis

--- routes/selections.js
+++ routes/selections.js
@@ -12,25 +12,85 @@ router.get("/", function (req, res) {
     });
 });
 
-//! GET a random selection based on the selected user's mood
+//! GET all selections of one mood
 
-router.get('/:mood', function(req, res, next) {
+router.get('/:mood', function(req, res) {
 
   const {mood} = req.params;
 
   models.Selection.findAll({
     where: {
       mood, 
-    }, 
-    order: sequelize.random('mood'),
-    limit: 1
+    },
   })
   .then(data => {
-    res.send(data[0]);
+    res.send(data);
   })
   .catch(err => res.status(500).send(err));
 
  });
+
+//! GET a random selection based on the selected user's mood
+
+// db(`SELECT * FROM responses WHERE mood = "${moodId}" ORDER BY RAND() LIMIT 1;`)
+
+// router.get('/:mood', function(req, res) {
+
+//   const {moodId} = req.params.mood;
+
+//   models.Selection.findAll({
+//     where: {
+//       moodId, 
+//     }, 
+//     order: sequelize.random('mood'),
+//     limit: 1
+//   })
+//   .then(data => {
+//     res.send(data[0]);
+//   })
+//   .catch(err => res.status(500).send(err));
+
+//  });
+
+// router.get('/:mood', function(req, res, next) {
+
+//   // 1. ensure params are pointing to each mood
+
+//   const {mood} = req.params.mood;
+
+//   models.Selection.findAll({
+
+//     // 2. Find column
+//     // 3. Have random fn executing in this column
+//     // 4. Limit get to 1 row
+
+//     attributes: ['mood',
+//     [sequelize.fn('RANDOM', sequelize.col('mood'))]
+//     ]})
+//   .then(data => {
+//     res.send(data[0]);
+//   })
+//   .catch(err => res.status(500).send(err));
+
+//  });
+
+// router.get('/:mood', function(req, res, next) {
+
+//   const {mood} = req.params;
+
+//   models.Selection.findAll({
+//     where: {
+//       mood, 
+//     }, 
+//     order: sequelize.random('mood'),
+//     limit: 1
+//   })
+//   .then(data => {
+//     res.send(data[0]);
+//   })
+//   .catch(err => res.status(500).send(err));
+
+//  });
 
 //! Create a new selection
 
