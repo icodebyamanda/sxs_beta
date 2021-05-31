3b65fcab14ba075545daa2aa60fde094a8573a5d
icodebyamanda <rasoamanda@gmail.com>
2021-05-31 16:38:52 +0200

    #40 user routes done

--- models/users.js
+++ models/users.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Users',
+    modelName: 'User',
   });
   return Users;
 };
--- routes/users.js
+++ routes/users.js
@@ -3,3 +3,44 @@ const { sequelize } = require("../models");
 var router = express.Router();
 const models = require("../models");
 
+//! Create a new user
+
+router.post('/', function(req, res) {
+  const { email, username, password } = req.body;
+  models.User.create({email, username, password})
+  .then((data) => {
+    res.send({message:'New User added'});
+  })
+  .catch((err) => {res.status(500).send(err)});
+});
+
+
+//! Get all users
+
+router.get('/', function(req, res) {
+
+models.User.findAll()
+  .then((data) => res.send(data))
+  .catch((error) => {
+    res.status(500).send(error);
+  });
+});
+
+//! Get one user
+
+router.get('/:userId', function(req, res) {
+  const {userId} = req.params;
+
+  models.User.findOne({
+    where: {
+      id: `${userId}`
+    },
+  })
+  .then(data => {
+    res.send(data);
+  })
+  .catch(err => res.status(500).send(err));
+})
+
+
+module.exports = router;
