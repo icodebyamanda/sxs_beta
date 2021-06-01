356c438390f5ddcd3a607120afbdc58f93b6c16a
icodebyamanda <rasoamanda@gmail.com>
2021-06-01 20:51:16 +0200

    #43 selections route cleaned up

--- routes/selections.js
+++ routes/selections.js
@@ -23,19 +23,6 @@ router.post("/", userShouldBeLoggedIn, async (req, res) => {
   }
 });
 
-/*
-
-//! Get all selections
-router.get("/", function (req, res) {
-  models.Selection.findAll()
-    .then((data) => res.send(data))
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-*/
-
 //! Get ONE user's all selections <- User's dashboard
 
 router.get('/list', userShouldBeLoggedIn, async (req, res) => {
@@ -103,11 +90,7 @@ router.get('/:mood', userShouldBeLoggedIn, async (req, res) => {
   }
 });
 
-//! Get ALL users' ALL selections
-
-//! Get ONE user's format segregation
-
-//! Delete ONE user's selection based on selection's id
+//! Delete ONE user's selection based on selection's id <- dashboard
 
 router.delete('/:itemId', userShouldBeLoggedIn, async (req, res) => {
   const UserId = req.user_id;
@@ -125,73 +108,6 @@ router.delete('/:itemId', userShouldBeLoggedIn, async (req, res) => {
   } catch(err) { 
     res.status(500).send(err)};
 })
-
-
-
-/*
-
-// Get all selections in random order
-
-// //! Get all selections
-// router.get("/", function (req, res) {
-//   models.Selection.findAll({
-//     // limit: 1,
-//     order: [
-//       [sequelize.fn('RANDOM')
-//     ]
-//     ]
-//   })
-//     .then((data) => res.send(data))
-//     .catch((error) => {
-//       res.status(500).send(error);
-//     });
-// });
-
-//! GET all selections of one mood
-
-router.get('/all/:mood', function(req, res) {
-
-  const {mood} = req.params;
-
-  models.Selection.findAll({
-    where: {
-      mood: `${mood}` 
-    },
-  })
-  .then(data => {
-    res.send(data);
-  })
-  .catch(err => res.status(500).send(err));
-
- });
-
-//! GET a random selection based on the selected user's mood
-
-// Translate from MYSQL -->> db(`SELECT * FROM responses WHERE mood = "${moodId}" ORDER BY RAND() LIMIT 1;`)
-
-router.get('/:mood', function(req, res) {
-
-  const {mood} = req.params;
-
-  models.Selection.findOne({
-    where: {
-      mood: `${mood}`
-    },
-
-    // Below is alternative
-    // order: models.sequelize.random(), limit: 1
-
-    order: sequelize.random(), limit: 1
-    
-  })
-  .then(data => {
-    res.send(data)[0];
-  })
-  .catch(err => res.status(500).send(err));
-
- });
-
-*/
 
 
 
