19f16d2beccecf72421887a26af5f7598dea5ad1
icodebyamanda <rasoamanda@gmail.com>
2021-06-11 09:49:54 +0200

    #48 Login.js token works but need to avoid console to print email and pswd

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,48 +1,77 @@
 import React, { useState } from "react";
+import axios from "axios";
 import Footer from './Footer';
 
 export default function LogIn() {
+
+  // const [loginDetails, setLogin] = useState({
+  //   email: '',
+  //   password: '',
+  // });
 
   const [loginDetails, setLogin] = useState({
     email: '',
     password: '',
   });
 
+  // const handleChange = (e) => {
+  //   e.persist()
+    
+  //   const value = e.target.value;
+
+  //   setLogin((state) => ({
+  //     ...state,
+  //     [e.target.name]: value,
+  //   }));
+  // };
+
   const handleChange = (e) => {
-    const value = e.target.value;
-
-    setLogin((state) => ({
-      ...state,
-      [e.target.name]: value,
-    }));
+    // e.preventDefault();
+    // e.persist();
+    console.log(e.target.name, e.target.value)
+    setLogin((state) => ({ ...state, [e.target.name]: e.target.value }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    logUserIn();
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   logUserIn();
+  // };
 
   const logUserIn = () => {
 
-    fetch('/users/login', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(loginDetails),
+    // when using syntax axios.post, data isn't used and second param is state's variable
+    axios.post('/users/login', loginDetails)
+    .then((result) => {
+      //store it locally - setItem a method of storage
+      localStorage.setItem("token", result.data.token);
+      console.log(result.data.message, result.data.token);
     })
-    .then(() => setLogin(loginDetails))
-    .catch((error) => { 
-      return error;
-    });
+    .catch((error) => console.log(error));
   };
+
+
+
+  // const logUserIn = () => {
+
+  //   axios('/users/login', {
+  //     method: 'POST',
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(loginDetails),
+  //   })
+  //   .then(() => setLogin(loginDetails))
+  //   .catch((error) => { 
+  //     return error;
+  //   });
+  // };
 
 
   return (
 
     <div>
 
-      <form onSubmit={handleSubmit}> 
+      {/* <form onSubmit={handleSubmit}>  */}
       
         <label>
           <div>Email</div>
@@ -64,14 +93,16 @@ export default function LogIn() {
           />
         </label>
 
-        <label>
+        <button onClick={logUserIn}> Log In</button>
+
+        {/* <label>
           <input 
             type="submit" 
             value="submit"
           />
-        </label>
+        </label> */}
       
-      </form>
+      {/* </form> */}
 
     <div><Footer /></div>
 
