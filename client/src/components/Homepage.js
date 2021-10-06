847de4b7fe58a7e903287e3721c78bc5504ffed6
icodebyamanda <rasoamanda@gmail.com>
2021-10-06 14:49:44 +0200

    #66 homepage text altered

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import UserName from "./UserName";
 import Footer from './Footer';
 
 export default function Homepage() {
@@ -9,8 +11,8 @@ export default function Homepage() {
 
     <header className="header" id="homepageHeader">
       <h1> Step By Step </h1>
-      <h2 id="HomepageSubheading">
-        <div> 'Vulnerability is the birthplace of innovation, creativity, and change'</div> <div> by Brené Brown </div>
+      <h2 className="header" id="HomepageSubheading">
+        Good morning and welcome <UserName /> 
       </h2> 
     </header>
 
@@ -20,18 +22,16 @@ export default function Homepage() {
         
         <div className="homepageText">
 
-          <h3 className="alignLeft hThreehomepageFontSize"> Welcome Friend, </h3>
+          
           <h4 className="alignLeft hFourhomepageFontSize"> This morning tool aims at helping you acknowledge your emotions, avoid denial for which you receive back some positive vibes to get you going with your day. </h4>
 
-          <p> In the <span className="spanHomepage"> 'Admin Space' </span>, store some links, videos or quotes, that inspire you, make you happy and make you feel good.<br></br>
+          <p> In the <span className="spanHomepage"><Link to='/Admin'> 'Admin Space'</Link> </span>, store some links, videos or quotes, that inspire you, make you happy and make you feel good.<br></br>
           You would need to match these links to the emotion you would like them to address to. </p>
 
-          <p> In <span className="spanHomepage"> 'Your Routine' </span>, select the emoticon that matches the closest your current emotions. <br></br>
+          <p> In <span className="spanHomepage"><Link to='/User'> 'Your Routine' </Link></span>, select the emoticon that matches the closest your current emotions. <br></br>
           In return, you will receive one of these links you have previously stored.  </p>
-         
-          <p className="spanHomepage"> Simple, right ?  </p>
           
-          <p> So jump out of bed and <span className="spanHomepage"> Step By Step, </span> embrace the day. </p>
+          <p> Now time to jump out of bed and <span className="spanHomepage"> Step By Step, </span> at your pace, embrace the day. </p>
         
         </div>
 
